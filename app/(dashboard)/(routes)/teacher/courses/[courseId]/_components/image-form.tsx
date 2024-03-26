b6be7613d03b2dd35b12776e265bc47cce476115
wassim-stack <werhaniwassim@kelibia.r-iset.tn>
2024-03-26 01:55:36 +0100

    finally uploadthing is working

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -2,16 +2,14 @@
 
 import * as z from "zod";
 import axios from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { Pencil, PlusCircle, ImageIcon } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
 import { Course } from "@prisma/client";
 import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
 
 interface ImageFormProps {
@@ -27,6 +25,7 @@ const formSchema = z.object({
 
 export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
   const [isEditing, setIsEditing] = useState(false);
+
   const toggleEdit = () => setIsEditing((current) => !current);
 
   const router = useRouter();
@@ -45,18 +44,18 @@ export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Course description
+        Course image
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing && <>Cancel</>}
           {!isEditing && !initialData.imageUrl && (
             <>
-              <PlusCircle className="w-4 h-4 mr-2" />
+              <PlusCircle className="h-4 w-4 mr-2" />
               Add an image
             </>
           )}
           {!isEditing && initialData.imageUrl && (
             <>
-              <Pencil className="w-4 h-4 mr-2" />
+              <Pencil className="h-4 w-4 mr-2" />
               Edit image
             </>
           )}
@@ -64,13 +63,13 @@ export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
       </div>
       {!isEditing &&
         (!initialData.imageUrl ? (
-          <div className="flex items-center justify-center h-60 bg-slate 200 rounded-md">
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
             <ImageIcon className="h-10 w-10 text-slate-500" />
           </div>
         ) : (
           <div className="relative aspect-video mt-2">
             <Image
-              alt="upload"
+              alt="Upload"
               fill
               className="object-cover rounded-md"
               src={initialData.imageUrl}
@@ -82,11 +81,15 @@ export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
           <FileUpload
             endpoint="courseImage"
             onChange={(url) => {
+              console.log(url);
               if (url) {
                 onSubmit({ imageUrl: url });
               }
             }}
           />
+          <div className="text-xs text-muted-foreground mt-4">
+            16:9 aspect ratio recommended
+          </div>
         </div>
       )}
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-const { withUt } = require("uploadthing/tw");
+import { withUt } from "uploadthing/tw";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = withUt({
@@ -57,20 +57,6 @@ module.exports = withUt({
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
